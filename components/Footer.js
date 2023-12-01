54e25cfe57f1412b4cf082514e04433da4762fdf
RomC0la <roma.dovgai1@gmail.com>
2023-12-01 15:41:41 +0200

    +footer, update slider

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import footerPuppy from '../img/footer-puppy.png'
+import mail from '../img/icon-email.svg'
+import phone from '../img/icon-phone.svg'
+import marker from '../img/icon-marker.png'
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className='footer'>
+                <div className='footer__container container'>
+                    <div className='footer__box'>
+                        <div className='footer__info'>
+                            <h3 className='footer__title'>For questions <br />and suggestions</h3>
+                            <a href="mailto:email@shelter.com" className='footer__text'>
+                                <img src={mail} alt='' />
+                                email@shelter.com
+                            </a>
+                            <a href="tel:+136745677554" className='footer__text'>
+                                <img src={phone} alt='' />
+                                +13 674 567 75 54
+                            </a>
+                        </div>
+                        <div className='footer__info'>
+                            <h3 className='footer__title'>We are waiting <br />for your visit</h3>
+                            <a href="google.com" className='footer__text'>
+                                <img src={marker} alt='' />
+                                1 Central Street, Boston (entrance from the store)
+                            </a>
+                            <a href="google.com" className='footer__text'>
+                                <img src={marker} alt='' />
+                                18 South Park, London
+                            </a>
+                        </div>
+                    </div>
+                    <img src={footerPuppy} alt='' className='footer__puppy' />
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
--- components/Slider.js
+++ components/Slider.js
@@ -1,51 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import pets from '../data/pets.json'
 import arrow from '../img/Arrow.svg'
 
-const Slider = () => {
-    return (
-        <div className='slider'>
-            <button className='slider__button slider__button--left' onClick={() => {
-                let card = document.querySelector("div.slider__card");
-                let container = document.querySelector("div.slider__content");
-                let left = container.offsetLeft;
-                let step = card.offsetWidth + parseInt(getComputedStyle(container).gap);
-                left = container.offsetLeft + step
-                container.style.left = `${left}px`;
-                if (left <= -card.offsetWidth) {
-                    document.querySelector("button.slider__button--right").removeAttribute("disabled", "disabled")
-                } else {
-                    document.querySelector("button.slider__button--left").setAttribute("disabled", "disabled")
-                }
-            }}><img src={arrow} alt='' /></button>
-            <div className='slider__container'>
-                <div className='slider__content'>
-                    {pets.map((el) => (
-                        <div className='slider__card' key={el.id} onClick={() => {
-                            alert("ASD")
-                        }}>
-                            <img src={el.img} alt='' className='pets__picture' />
-                            <p className='pets__name'>{el.name}</p>
-                            <button className='pets__learnmore'>Learn More</button>
-                        </div>
-                    ))}
+class Slider extends Component {
+    render() {
+        return (
+            <div className='slider'>
+                <button className='slider__button slider__button--left' ><img src={arrow} alt='' /></button>
+                <div className='slider__container'>
+                    <div className='slider__content'>
+                        {pets.map((el) => (
+                            <div className='slider__card' key={el.id} onClick={() => {
+                                alert("ASD")
+                            }}>
+                                <img src={el.img} alt='' className='pets__picture' />
+                                <p className='pets__name'>{el.name}</p>
+                                <button className='pets__learnmore'>Learn More</button>
+                            </div>
+                        ))}
+                    </div>
                 </div>
+                <button className='slider__button slider__button--right' ><img src={arrow} alt='' /></button>
             </div>
-            <button className='slider__button slider__button--right' onClick={() => {
-                let card = document.querySelector("div.slider__card");
-                let container = document.querySelector("div.slider__content");
-                let left = container.offsetLeft;
-                let step = card.offsetWidth + parseInt(getComputedStyle(container).gap);
-                left = container.offsetLeft - step
-                container.style.left = `${left}px`;
-                if (left >= - ((pets.length - 2) * 270)) {
-                    document.querySelector("button.slider__button--left").removeAttribute("disabled", "disabled")
-                } else {
-                    document.querySelector("button.slider__button--right").setAttribute("disabled", "disabled")
-                }
-            }}><img src={arrow} alt='' /></button>
-        </div>
-    );
+        );
+    }
+    componentDidMount() {
+        window.addEventListener("resize", () => {
+            let containerWidth = document.querySelector("div.slider__container").offsetWidth;
+            let cardWidth = 270;
+            let amountOfCards = Math.floor(containerWidth / cardWidth);
+            let gap;
+            amountOfCards > 1 ? gap = (containerWidth - (amountOfCards * cardWidth)) / (amountOfCards - 1) : gap = (containerWidth - (amountOfCards * cardWidth));
+            console.log(gap)
+        })
+    }
 }
 
 export default Slider;
